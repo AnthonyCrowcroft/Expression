70dc84cb9e79d840e9d028c138645ed10ab35149
Anthony <anthonycrowcroft@hotmail.com>
2015-08-05 15:31:52 +1200

    template contact

--- server/contact/contact.config.js
+++ server/contact/contact.config.js
@@ -3,3 +3,14 @@
  * Package:     Contact
  * Created:     04/08/2015.
  */
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport();
+
+transporter.sendMail({
+    from: 'sender@address',
+    to: 'receiver@address',
+    subject: 'hello',
+    text: 'hello world!'
+});
