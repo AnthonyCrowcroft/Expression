f4a8ffaa98b2e0926405823af286409911bbf813
Anthony Crowcroft <anthonycrowcroft@Anthonys-iMac.telecom>
2015-08-16 17:30:33 +1200

    login tidied up ready future functionality

--- public/app/pages/contact/contact.controller.js
+++ public/app/pages/contact/contact.controller.js
@@ -7,9 +7,9 @@
  */
 
 angular.module("expApp")
-    .controller("ContactController", function($location, $rootScope, $http, contactData){
+    .controller("ContactController", function($location, $rootScope, $http){
 
-        this.data = contactData.data;
-        $rootScope.currentPage = contactData.data;
+        //this.data = contactData.data;
+        //$rootScope.currentPage = contactData.data;
 
     });
--- public/app/users/login/login.directive.js
+++ public/app/users/login/login.directive.js
@@ -14,42 +14,45 @@ angular.module("expApp")
             templateUrl: '/app/users/login/login.html',
             controller: function($http, $rootScope){
                 this.user = {};
-                this.signIn = function(){
-                    if (this.user.email && this.user.password) {
-                        $http.post('/local/login', {
-                            "email": this.user.email,
-                            "password": this.user.password
-                        }).then(function(response){
-                            if(response) {
-                                if (response.data.user != false) {
-                                    $rootScope.user = response.data.user;
-                                }
-                            }
-                        });
-                    } else {
-                        console.log("A field did not pass validation");
-                    }
-                };
-                this.signUp = function(){
-                    if (this.user.email && this.user.password && this.user.passConfirm) {
-                        if (this.user.password == this.user.passConfirm) {
-                            $http.post('/local/signup', {
+                this.submit = function() {
+                    if($rootScope.loginState == 'Login') {
+                        if (this.user.email && this.user.password) {
+                            $http.post('/local/login', {
                                 "email": this.user.email,
-                                "password": this.user.password,
-                                "name": this.user.name
-                            }).then(function(response){
-                                if(response) {
-                                    $rootScope.alerts.push(response.data);
+                                "password": this.user.password
+                            }).then(function (response) {
+                                if (response) {
+                                    if (response.data.user != false) {
+                                        $rootScope.user = response.data.user;
+                                    }
                                 }
-
                             });
                         } else {
-                            console.log("Passwords don't match");
+                            console.log("A field did not pass validation");
                         }
-                    } else {
-                        console.log("Email didn't pass validation");
                     }
-                };
+                    if($rootScope.loginState == 'Register') {
+                        if (this.user.email && this.user.password && this.user.passConfirm) {
+                            if (this.user.password == this.user.passConfirm) {
+                                $http.post('/local/signup', {
+                                    "email": this.user.email,
+                                    "password": this.user.password,
+                                    "name": this.user.name
+                                }).then(function(response){
+                                    if(response) {
+                                        $rootScope.alerts.push(response.data);
+                                    }
+
+                                });
+                            } else {
+                                console.log("Passwords don't match");
+                            }
+                        } else {
+                            console.log("Email didn't pass validation");
+                        }
+                    }
+                }
+
             },
             controllerAs: 'login'
         };
